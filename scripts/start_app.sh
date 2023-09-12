71c4069efda1fb106bdfebe3349c506293357edf
poonamk3 <poonamk@thoughtwin.com>
2023-09-12 18:41:58 +0530

    D

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/bash 
-
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
-
-python manage.py migrate 
-python manage.py makemigrations     
-python manage.py collectstatic
+source /home/ubuntu/env/bin/activate
+cd /home/ubuntu/blogprojectdrf
+python manage.py migrate
+python manage.py collectstatic --noinput
+sudo systemctl daemon-reload
 sudo service gunicorn restart
 sudo service nginx restart
-#sudo tail -f /var/log/nginx/error.log
-#sudo systemctl reload nginx
-#sudo tail -f /var/log/nginx/error.log
-#sudo nginx -t
-#sudo systemctl restart gunicorn
-#sudo systemctl status gunicorn
-#sudo systemctl status nginx
-# Check the status
-#systemctl status gunicorn
-# Restart:
-#systemctl restart gunicorn
 #sudo systemctl status nginx
