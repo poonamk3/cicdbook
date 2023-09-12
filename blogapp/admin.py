84cd12bc90a0526168cda1a5ac24d495e45a61f4
poonamk3 <poonamk@thoughtwin.com>
2023-09-12 17:16:21 +0530

    add blog

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -5,6 +5,7 @@ from . import models
 # Register your models here.
 
 @admin.register(models.Blog)
+@admin.register(models.Title)
 
 # The BlogAdmin class is a subclass of the admin.ModelAdmin class. 
 class BlogAdmin(admin.ModelAdmin):
@@ -27,3 +28,21 @@ class BlogAdmin(admin.ModelAdmin):
     ]
 
 
+class TitleAdmin(admin.ModelAdmin):
+    
+    fields = [
+        
+        'user',
+        'title',
+        'blog_text',
+        'main_image',
+    ]
+    list_display = [
+        'title', 
+        'user',
+        'blog_text',
+        'main_image',
+        'created_at',
+        'updated_at',
+        
+    ]
--- blogapp/models.py
+++ blogapp/models.py
@@ -24,3 +24,12 @@ class Blog(BaseModel):
         
     def __str__(self):
         return self.title
+
+class Title(BaseModel):
+    user =  models.ForeignKey(User, related_name='blogs', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    blog_text= models.TextField()  
+    main_image = models.FileField()   
+        
+    def __str__(self):
+        return self.title
